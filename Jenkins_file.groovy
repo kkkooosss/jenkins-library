814a1d0597e3ef4ee4f89dc59d5e007026017ffb
konstantin.kachar <konstantin.kachar@comm-it.com>
2024-07-03 14:42:42 +0300

    var test

---
+++ Jenkins_file.groovy
@@ -0,0 +1,37 @@
+@Library("shared-lib-pipe") _
+
+pipeline {
+    agent {
+        label 'linux'
+    }
+    parameters {
+        string(name: 'VAR_00', defaultValue: '000', description: 'Variable 00')
+    }
+    environment {
+        var_00 = "${params.VAR_00 ?: '000'}"
+        var_01 = ""
+        var_02 = ""
+        var_03 = ""
+    }
+    stages {
+        stage("Preparations") {
+            steps {
+                script {
+                    preparations000()
+                    echo "this is Preparation stage."
+                    echo "var_00: ${env.var_00}"
+                    echo "var_01: ${env.var_01}"
+                    echo "var_02: ${env.var_02}"
+                    echo "var_03: ${env.var_03}"
+                }
+            }
+        }
+        stage("Create ECR REPO if not exist") {
+            steps {
+                script {
+                    create_ecr_repo(env.var_00, env.var_01, env.var_02)
+                }
+            }
+        }
+    }
+}
--- vars/preparations000.groovy
+++ vars/preparations000.groovy
@@ -1,35 +1,17 @@
 def call() {
     if (env.var_00 == "000") {
-        sh """
-            echo this is Preparation stage 000.
-            export var_01=10000
-            export var_02=20000
-            export var_03=30000
-            echo \${var_01}
-            echo \${var_02}
-            echo \${var_03}
-        """
+        env.var_01 = "10000"
+        env.var_02 = "20000"
+        env.var_03 = "30000"
     } else if (env.var_00 == "111") {
-        sh """
-            echo this is Preparation stage 111.
-            export var_01=11111
-            export var_02=21111
-            export var_03=31111
-            echo \${var_01}
-            echo \${var_02}
-            echo \${var_03}
-        """
+        env.var_01 = "11111"
+        env.var_02 = "21111"
+        env.var_03 = "31111"
     } else if (env.var_00 == "222") {
-        sh """
-            echo this is Preparation stage 222.
-            export var_01=12222
-            export var_02=22222
-            export var_03=32222
-            echo \${var_01}
-            echo \${var_02}
-            echo \${var_03}
-        """
+        env.var_01 = "12222"
+        env.var_02 = "22222"
+        env.var_03 = "32222"
     } else {
-        sh "echo *******ERROR******"
+        error "Invalid value for var_00: ${env.var_00}"
     }
 }
